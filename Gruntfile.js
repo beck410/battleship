2f827842c616480df28483b7bf723b0a2c2fbf17
beck410 <beck410@hotmail.com>
2014-11-22 22:30:58 -0600

    set up gruntfile tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,110 @@
-'use strict';
+
 
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
     cssmin: {
       my_target: {
         files: [{
           expand: true,
-          cwd: 'app/styles/',
+          cwd: 'app/styles',
           src: ['*.css','!*.min.css'],
-          dest: 'app/dist/',
+          dest: 'app/dist/styles/',
           ext: '.min.css' 
         }]
+      },
+      // combine: {
+      //   files: {
+      //     'app/dist/styles/<%= pkg.name =%>---<%= pkg.version =%>.min.css': ['app/styles/min/*.min.css']
+      //   }
+      // }
+    },
+
+    uglify: {
+      options: {
+        manage: false
+      },
+      my_target: {
+        files: {
+          'app/dist/js/<%= pkg.name %<.js': ['app/js/<%= pkg.name %>.js'] 
+        }
       }
+    },
+    
+    concat: {
+      options: {
+        seperator: ';',
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - '+ '<% grunt.template.today("yyy-mm-dd") %>' + '*/'
+        },
+      dist: {
+        src: ['app/js/*.js'],
+        dest: 'app/js/<%= pkg.name %>.js'
+        }
+      },
+
+    sass: {
+      options: {
+        compress: false,
+        sourcemap: 'none'
+      },
+      dist: {
+        files: {
+          'app/styles/<%= pkg.name %>.css': 'app/sass/*.scss'
+          }  
+      }
+    },
+
+    watch: {
+     sass: {
+      files: ['app/sass/*.scss'],
+      tasks: ['sass', 'cssmin','uncss']
+     }
+    },
+
+    uncss: {
+      dist: {
+        files: {
+          'app/dist/styles/*.css': ['app/*.html']
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'app/images/',
+          src: [ '*.png', '*.svg', '*.jpg' ],
+          dest: 'app/dist/images'
+        }]
+      }
+    },
+
+    notify_hooks: {
+      options: {
+        enable: true,
+        max_jshint_notifications: 5,
+      },
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish'),
+      },
+      target: ['Gruntfile.js', 'app/js/**/*.js','test/*.js']
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-}
+  
+  grunt.task.run('notify_hooks');
+
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('css', ['sass,cssmin,uncss']);
+  grunt.registerTask('production', ['sass,cssmin,uncss','uglify','imagemin']);
+  
+};
