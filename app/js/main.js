f8a1c2f4beb2e35495c8576c2f95c65731aabc6c
beck410 <beck410@hotmail.com>
2014-11-25 11:14:10 -0600

    added data-position attribute to grid tds

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,48 @@
 ;(function(){
  'use strict'; 
-//click events
- $('.ship').click(function(){
-    $('.ship').removeClass('selected');
-    $(this).addClass('selected');
+
+  $().ready(function(){
+    //click events
+    $('.ship').click(function(){
+      $('.ship').removeClass('selected');
+      $(this).addClass('selected');
+    });
+
+    $('#rotate').click(function(){
+    var clickedShip = $('.selected');
+    var dataShip = clickedShip.attr('data-ship');
+
+    clickedShip.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
+    });
+
+     $('#shipsReady').click(function(){
+       $('.ship').draggable('destroy');
+     });
+
+    $('.ship').draggable({ 
+      grid: [50,50],
+      revert: 'invalid',
+      snap: 'td',
+      handle: '#arrow'
+    });
+
+    $('.player-grid table td').droppable({
+      greedy: true,
+      drop: function(event, ui){
+        var shipType = findShipType(ui.draggable);
+        console.log(shipType);
+        console.log(ui.draggable)
+        $(this).addClass('hasShip');
+      },
+      out: function(){
+        $(this).removeClass('hasShip');
+      }
+    });
+
+  //main function
+
   });
-
-  $('#rotate').click(function(){
-  var clickedShip = $('.selected');
-  var dataShip = clickedShip.attr('data-ship');
-
-  clickedShip.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
-  });
-
-   $('#shipsReady').click(function(){
-     $('.ship').draggable('destroy');
-   });
-
-   //main functions
-  $('.ship').draggable({ 
-    grid: [50,50],
-    revert: 'invalid',
-    snap: 'td',
-    handle: '#arrow'
-  });
-  $('.player-grid table td').droppable({
-    greedy: true,
-    drop: function(event, ui){
-      var shipType = findShipType(ui.draggable);
-      console.log(shipType);
-      console.log($(this));
-      $(this).addClass('hasShip');
-    },
-    out: function(){
-      $(this).removeClass('hasShip');
-    }
-  });
-
+  
   //helper functions
   function findShipType(draggable){
     var shipClasses = ['carrier-horizontal','battleship-horizontal', 'cruiser-horizontal', 'submarine-horizontal', 'destroyer-horizontal', 'carrier-vertical', 'battleship-vertical', 'cruiser-vertical','submarine-vertical', 'destroyer-vertical'];
