b23d383575ebc8e3962301809e8e23eb729682e3
beck410 <beck410@hotmail.com>
2014-11-24 21:53:30 -0600

    added function to find draggable's ship type and orientation

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,50 @@
 ;(function(){
  'use strict'; 
-  $('.ship').click(function(){
+//click events
+ $('.ship').click(function(){
     $('.ship').removeClass('selected');
     $(this).addClass('selected');
-  })
+  });
 
   $('#rotate').click(function(){
   var clickedShip = $('.selected');
   var dataShip = clickedShip.attr('data-ship');
 
   clickedShip.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
-  })
+  });
 
+   $('#shipsReady').click(function(){
+     $('.ship').draggable('destroy');
+   });
+
+   //main functions
   $('.ship').draggable({ 
     grid: [50,50],
     revert: 'invalid',
     snap: 'td',
   });
   $('.player-grid table td').droppable({
+    greedy: true,
     drop: function(event, ui){
+      var shipType = findShipType(ui.draggable);
+      console.log(shipType);
       $(this).addClass('hasShip');
     },
-    out: function(event, ui){
-      $(this).removeClass('hasShip')
+    out: function(){
+      $(this).removeClass('hasShip');
     }
   });
 
-  //remove draggable when readyFire clicked
-   $('#shipsReady').click(function(){
-     shipArray();
-     $('.ship').draggable('destroy');
-   });
+  //helper functions
+  function findShipType(draggable){
+    var shipClasses = ['carrier-horizontal','battleship-horizontal', 'cruiser-horizontal', 'submarine-horizontal', 'destroyer-horizontal', 'carrier-vertical', 'battleship-vertical', 'cruiser-vertical','submarine-vertical', 'destroyer-vertical'];
+    var shipClass;
+    shipClasses.forEach(function(className){
+      if(draggable.hasClass(className)){
+        shipClass = className;
+        return;
+      }
+    });
+    return shipClass;
+  }
 })();
