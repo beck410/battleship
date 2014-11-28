ca4c156ed66aaacd443993c2753746aaaa87ecb4
beck410 <beck410@hotmail.com>
2014-11-28 12:11:27 -0600

    two player functionality - adding selected to clicked ship added

--- app/js/main.js
+++ app/js/main.js
@@ -1,164 +1,164 @@
-//;(function(){
- 'use strict'; 
-  //jshint jquery: true
-  $().ready(function(){
-    //click events
-    //class of selected added to ship when clicked
-    $('.ship').click(function(){
-      $('.ship').removeClass('selected');
-      $(this).addClass('selected');
-    });
-    
-    //.selected is rotated when button clicked
-    $('#rotate').click(function(){
-      var clickedShip = $('.selected');
-      var shipsCell = $('.dropped');
-      var dataShip = clickedShip.attr('data-ship');
-      clickedShip.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
-      shipsCell.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
-      var shipType = findShipType(clickedShip)
-      setShipSections(shipType);
-    });
-     
-      //ui.draggable removed from ships when 'Fleet Ready' button clicked
-     $('#shipsReady').click(function(){
-       $('.ship').draggable('destroy');
-     });
-    
-     //adds ui.draggable to ships
-    $('.ship').draggable({ 
-      grid: [20,20],
-      revert: 'invalid',
-      snap: 'td',
-      handle: '#arrow',
-    });
-    
-    //adds ui.droppable to table's td
-    $('.grids table td').droppable({
-      greedy: true,
-      drop: function(event, ui){
-        var shipType = findShipType(ui.draggable);
-        $('.grids td').removeClass(shipType + ' dropped');
-        $(this).addClass(shipType + ' dropped');
-        setShipSections(shipType);
-      },
-      out: function(){
-        $(this).removeClass('hasShip');
-      },
-      tolerance: 'pointer'
-    });
-  });
-  
-  //helper functions
-  //finds ship type class for selected ship
-  function findShipType(draggable){
-    var shipClasses = ['carrier-horizontal','battleship-horizontal', 'cruiser-horizontal', 'submarine-horizontal', 'destroyer-horizontal', 'carrier-vertical', 'battleship-vertical', 'cruiser-vertical','submarine-vertical', 'destroyer-vertical'];
-    var shipClass;
-    shipClasses.forEach(function(ship){
-      if(draggable.hasClass(ship)){
-        shipClass = ship;
-        return;
-      }
-    });
-    return shipClass;
-  }
-
-  //checks ship's type class and runs its function to add all parts of ship
-  function setShipSections(ship){
-    var cell = $('td.' + ship);
-    var shipPosition = cell.attr('data-position');
-    switch (ship){
-      case 'carrier-horizontal':
-      case 'carrier-vertical':
-        shipSections(cell,shipPosition,ship,5);
-        break;
-      case 'battleship-horizontal':
-      case 'battleship-vertical':
-        shipSections(cell,shipPosition,ship,4);
-        break;
-      case 'cruiser-horizontal':
-      case 'cruiser-vertical':
-       shipSections(cell,shipPosition,ship,3);
-        break;
-      case 'submarine-horizontal':
-      case 'submarine-vertical':
-       shipSections(cell,shipPosition,ship,3);
-       break;
-      case 'destroyer-horizontal':
-      case 'destroyer-vertical':
-       shipSections(cell,shipPosition,ship,2);
-        break;
-    }
-  }
-
-  //adds classes to ship's tds
-  function shipSections(shipFront, position,shipType,sectionLength){
-    position = +(position);
-    var shipName = splitShipType(shipType);
-    if(shipType.indexOf('vertical') === -1){
-      var neighborCells = createShipSectionArray(sectionLength,'horizontal', position);
-      removeShipClass(shipName);
-      addClassToNeighbors(neighborCells,shipType);
-      return;
-    } else {      
-      var neighborCells = createShipSectionArray(sectionLength, 'vertical', position); 
-      removeShipClass(shipName);
-      addClassToNeighbors(neighborCells,shipType);
-      }
-  }
-  
-  //removes ship class from td when dropped again
-  function removeShipClass(ship){
-    $('td').removeClass(ship + '-horizontal');
-    $('td').removeClass(ship + '-vertical');
-  }
-  //adds ship class to ship's sections
-  function addClassToNeighbors(neighbors,ship){
-    var n=1;
-    neighbors.forEach(function(dataPosition){
-      var neighbor = $('td[data-position="' + dataPosition + '"]'); 
-      neighbor.addClass(ship);
-      n++;
-    });
-  }
-
-  //splits ship class into type and orientation
-  function splitShipType(shipType){
-    var shipTypeArray = shipType.split('-');
-    var shipName = shipTypeArray[0];
-    return shipName;
-  }
-  
-  //finds ship's sections
-  function createShipSectionArray(shipLength, orientation, cell){
-    var sectionArray = [];
-    var counter = 0;
-    if(orientation === 'horizontal'){
-      while(sectionArray.length <  shipLength ){
-        var neighbor = cell + counter;
-        counter ++;
-        sectionArray.push(neighbor);
-      }
-    } else {
-      while(sectionArray.length < shipLength) {
-        var neighbor = cell + counter;
-          counter += 10;
-        sectionArray.push(neighbor);
-      }
-    }
-    return sectionArray;
-  }
-  
-  //check if any ship sections are outside grid boundary - NOT USED YET
-    function checkGridBoundaries(shipSections){
-    var outsideGrid = true;
-    var n = 0;
-    while(outsideGrid === true && n<shipSections.length){
-      if(!(shipSections[n] > 0 && shipSections[n] < 101)){
-        outsideGrid = false; 
-      }
-    n++;
-    }
-    return outsideGrid;
-  }
-//})();
+// //;(function(){
+//  'use strict';
+//   //jshint jquery: true
+//   $().ready(function(){
+//     //click events
+//     //class of selected added to ship when clicked
+//     $('.player-one-grid .ship').click(function(){
+//       $('.player-one-grid .ship').removeClass('selected');
+//       $(this).addClass('selected');
+//     });
+//
+//     //.selected is rotated when button clicked
+//     $('#rotate').click(function(){
+//       var clickedShip = $('.selected');
+//       var shipsCell = $('.dropped');
+//       var dataShip = clickedShip.attr('data-ship');
+//       clickedShip.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
+//       shipsCell.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
+//       var shipType = findShipType(clickedShip)
+//       setShipSections(shipType);
+//     });
+//
+//       //ui.draggable removed from ships when 'Fleet Ready' button clicked
+//      $('#shipsReady').click(function(){
+//        $('.ship').draggable('destroy');
+//      });
+//
+//      //adds ui.draggable to ships
+//     $('.ship').draggable({
+//       grid: [20,20],
+//       revert: 'invalid',
+//       snap: 'td',
+//       handle: '#arrow',
+//     });
+//
+//     //adds ui.droppable to table's td
+//     $('.grids table td').droppable({
+//       greedy: true,
+//       drop: function(event, ui){
+//         var shipType = findShipType(ui.draggable);
+//         $('.grids td').removeClass(shipType + ' dropped');
+//         $(this).addClass(shipType + ' dropped');
+//         setShipSections(shipType);
+//       },
+//       out: function(){
+//         $(this).removeClass('hasShip');
+//       },
+//       tolerance: 'pointer'
+//     });
+//   });
+//
+//   //helper functions
+//   //finds ship type class for selected ship
+//   function findShipType(draggable){
+//     var shipClasses = ['carrier-horizontal','battleship-horizontal', 'cruiser-horizontal', 'submarine-horizontal', 'destroyer-horizontal', 'carrier-vertical', 'battleship-vertical', 'cruiser-vertical','submarine-vertical', 'destroyer-vertical'];
+//     var shipClass;
+//     shipClasses.forEach(function(ship){
+//       if(draggable.hasClass(ship)){
+//         shipClass = ship;
+//         return;
+//       }
+//     });
+//     return shipClass;
+//   }
+//
+//   //checks ship's type class and runs its function to add all parts of ship
+//   function setShipSections(ship){
+//     var cell = $('td.' + ship);
+//     var shipPosition = cell.attr('data-position');
+//     switch (ship){
+//       case 'carrier-horizontal':
+//       case 'carrier-vertical':
+//         shipSections(cell,shipPosition,ship,5);
+//         break;
+//       case 'battleship-horizontal':
+//       case 'battleship-vertical':
+//         shipSections(cell,shipPosition,ship,4);
+//         break;
+//       case 'cruiser-horizontal':
+//       case 'cruiser-vertical':
+//        shipSections(cell,shipPosition,ship,3);
+//         break;
+//       case 'submarine-horizontal':
+//       case 'submarine-vertical':
+//        shipSections(cell,shipPosition,ship,3);
+//        break;
+//       case 'destroyer-horizontal':
+//       case 'destroyer-vertical':
+//        shipSections(cell,shipPosition,ship,2);
+//         break;
+//     }
+//   }
+//
+//   //adds classes to ship's tds
+//   function shipSections(shipFront, position,shipType,sectionLength){
+//     position = +(position);
+//     var shipName = splitShipType(shipType);
+//     if(shipType.indexOf('vertical') === -1){
+//       var neighborCells = createShipSectionArray(sectionLength,'horizontal', position);
+//       removeShipClass(shipName);
+//       addClassToNeighbors(neighborCells,shipType);
+//       return;
+//     } else {
+//       var neighborCells = createShipSectionArray(sectionLength, 'vertical', position);
+//       removeShipClass(shipName);
+//       addClassToNeighbors(neighborCells,shipType);
+//       }
+//   }
+//
+//   //removes ship class from td when dropped again
+//   function removeShipClass(ship){
+//     $('td').removeClass(ship + '-horizontal');
+//     $('td').removeClass(ship + '-vertical');
+//   }
+//   //adds ship class to ship's sections
+//   function addClassToNeighbors(neighbors,ship){
+//     var n=1;
+//     neighbors.forEach(function(dataPosition){
+//       var neighbor = $('td[data-position="' + dataPosition + '"]');
+//       neighbor.addClass(ship);
+//       n++;
+//     });
+//   }
+//
+//   //splits ship class into type and orientation
+//   function splitShipType(shipType){
+//     var shipTypeArray = shipType.split('-');
+//     var shipName = shipTypeArray[0];
+//     return shipName;
+//   }
+//
+//   //finds ship's sections
+//   function createShipSectionArray(shipLength, orientation, cell){
+//     var sectionArray = [];
+//     var counter = 0;
+//     if(orientation === 'horizontal'){
+//       while(sectionArray.length <  shipLength ){
+//         var neighbor = cell + counter;
+//         counter ++;
+//         sectionArray.push(neighbor);
+//       }
+//     } else {
+//       while(sectionArray.length < shipLength) {
+//         var neighbor = cell + counter;
+//           counter += 10;
+//         sectionArray.push(neighbor);
+//       }
+//     }
+//     return sectionArray;
+//   }
+//
+//   //check if any ship sections are outside grid boundary - NOT USED YET
+//     function checkGridBoundaries(shipSections){
+//     var outsideGrid = true;
+//     var n = 0;
+//     while(outsideGrid === true && n<shipSections.length){
+//       if(!(shipSections[n] > 0 && shipSections[n] < 101)){
+//         outsideGrid = false;
+//       }
+//     n++;
+//     }
+//     return outsideGrid;
+//   }
+// //})();
--- app/js/start.js
+++ app/js/start.js
@@ -1,13 +1,31 @@
 ;(function(){
   'use strict';
-  $('#player-one').click(function(){
-    console.log("pig")
-    $('.player-one-grids').css('display','block');
-});
-$('#player-two').click(function(){
-  $('.player-two-grids').css('display','block');
-});
+  //jshint jquery: true
+  $().ready(function(){
 
+    //click events
+    //shows player one grids
+    $('#player-one').click(function(){
+      var playerOneGrids = '.player-one-grids';
+      $(playerOneGrids).css('display','block');
+      playerStart(playerOneGrids);
+    });
+
+    //shows player two grids
+    $('#player-two').click(function(){
+      var playerTwoGrids = '.player-two-grids';
+      $(playerTwoGrids).css('display','block');
+      playerStart(playerTwoGrids);
+    });
+  });
+
+  //HELPER FUNCTIONS
+  //sets ships on grid
+  function playerStart(playerGrid){
+    $(playerGrid + ' .ship').click(function(){
+      console.log("1");
+      $(playerGrid + ' .ship').removeClass('selected');
+      $(this).addClass('selected');
+    });
+  } 
 })();
-
-
