6911dec04aa21268bcacff2f9f9288b1e0b08cfe
beck410 <beck410@hotmail.com>
2014-11-28 17:38:25 -0600

    two player rotating ships working

--- app/js/start.js
+++ app/js/start.js
@@ -9,6 +9,7 @@
       var playerOneGrids = '.player-one-grids';
       $(playerOneGrids).css('display','block');
       playerStart(playerOneGrids);
+      rotateShips(playerOneGrids);
     });
 
     //shows player two grids
@@ -16,6 +17,7 @@
       var playerTwoGrids = '.player-two-grids';
       $(playerTwoGrids).css('display','block');
       playerStart(playerTwoGrids);
+      rotateShips(playerTwoGrids);
     });
   });
 
@@ -23,9 +25,36 @@
   //sets ships on grid
   function playerStart(playerGrid){
     $(playerGrid + ' .ship').click(function(){
-      console.log("1");
       $(playerGrid + ' .ship').removeClass('selected');
       $(this).addClass('selected');
     });
-  } 
+  }
+
+  function rotateShips(playerGrid){
+    $(playerGrid + ' #rotate').click(function(){
+       var clickedShip = $(playerGrid + ' .selected');
+       var shipsCell = $('.dropped');
+       var dataShip = clickedShip.attr('data-ship');
+       clickedShip.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
+       //shipsCell.toggleClass(dataShip + '-horizontal').toggleClass(dataShip + '-vertical');
+       //console.log(clickedShip)
+       var shipType = findShipType(clickedShip);
+       console.log(shipType);
+       //setShipSections(shipType);
+    });
+ };
+  
+  //finds ship type class for selected ship
+  function findShipType(clickedShip){
+    var shipClasses = ['carrier-horizontal','battleship-horizontal', 'cruiser-horizontal', 'submarine-horizontal', 'destroyer-horizontal', 'carrier-vertical', 'battleship-vertical', 'cruiser-vertical','submarine-vertical', 'destroyer-vertical'];
+    var shipClass;
+    shipClasses.forEach(function(ship){
+      if(clickedShip.hasClass(ship)){
+        shipClass = ship;
+        return;
+      }
+    });
+    return shipClass;
+  }
+
 })();
